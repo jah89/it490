a8d2b75b4526d16af2464150369ac7b23fa9ea6c
jah89 <jah89@njit.edu>
2024-10-03 09:56:51 -0400

    test producer code fix

--- RabbitMQ/testProducer.php
+++ RabbitMQ/testProducer.php
@@ -1,13 +1,13 @@
 <?php 
-require_once(__DIR__ . '/../../../RabbitMQ/path.inc');
-require_once(__DIR__ . '/../../../RabbitMQ/get_host_info.inc');
-require_once(__DIR__ . '/../../../RabbitMQ/rabbitMQLib.inc');
+require_once(__DIR__ . '/path.inc');
+require_once(__DIR__ . '/get_host_info.inc');
+require_once(__DIR__ . '/rabbitMQLib.inc');
 require '/vendor/autoload.php'; 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 // Parse the ini file to get the RabbitMQ configuration
-$config = parse_ini_file(__DIR__ . '/../../../RabbitMQ/testRabbitMQ.ini', true);
+$config = parse_ini_file(__DIR__ . '/testRabbitMQ.ini', true);
 $authConfig = $config['Authentication'];
 
 //connection with RabbitMQ
