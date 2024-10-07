6929030542b856e9fa942d11c5308d55aab67ad2
macho <mmgajda@gmail.com>
2024-10-06 23:33:26 -0400

    ..

---
+++ db/userToken.php
@@ -0,0 +1,26 @@
+<?php
+require 'Session.inc.php';
+
+
+//echo uniqid() . "\n";
+//echo time() ."\n";
+
+
+$token = uniqid();
+$timestamp = time() + (3 * 3600);
+$session = new Session($token, $timestamp, "bob");
+$loginResponse = new LoginResponse(true, $session);
+
+//$loginResponse = new LoginResponse(true, null);
+
+/*
+consuming LoginRequest Object of Rabbit
+
+then use getEMail function to use in session instance  Ex. session = new Session(getEmail(), $token, $timestamp);
+
+
+
+
+
+
+?>
