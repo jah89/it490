f69733f10d9ffb6bc5699fc824ec100bccde1f42
enisakil <ea377@njit.edu>
2024-10-17 03:43:22 +0000

    Made changes to schema file to reflect changes in session and users table

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,22 @@ CREATE DATABASE nba; --db name is work in progress
 USE nba;
 
 -- Create user 'miz' and grant privileges
-CREATE USER 'miz'@'172.30.97.102' IDENTIFIED BY 'teamfantasy';
-GRANT ALL PRIVILEGES ON nba.* TO 'miz'@'172.30.97.102';
+CREATE USER 'eait490'@'172.30.17.239' IDENTIFIED BY 'teamfantasy';
+GRANT ALL PRIVILEGES ON nba.* TO 'eait490'@'172.30.17.239';
 
 -- Create the 'users' table
 CREATE TABLE users (
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Create the 'sessions' table
 CREATE TABLE sessions (
+    session_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     session_token VARCHAR(255) NOT NULL,
-    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    timestamp INT NOT NULL,
     user_id INT NOT NULL,
     email VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
